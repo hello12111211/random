971d8df1f9de326ed7ca31ee357167f063db342d
hello12111211 <thomas111222111111@outlook.com>
2025-05-02 09:38:29 +0100

    Create about:blank.js

---
+++ about:blank.js
@@ -0,0 +1,26 @@
+<head>
+    <button onclick="openGame()">Free Vbux</button>
+    <script>
+        
+    function openGame() {
+        var win = window.open();
+        var url = "";       //put url here
+        var hide = '<title>about:blank</title><link rel="icon" type="image/x-icon" href="https://clcem1211.onrender.com/Login.ico">';
+
+        // Insert the title and favicon into the new window's document
+        win.document.head.innerHTML = hide;
+        
+        // Create iframe and set attributes
+        var iframe = win.document.createElement('iframe');
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+        iframe.style.border = "none";
+        iframe.src = url;
+        
+        // Append iframe to the window's document body
+        win.document.body.appendChild(iframe);
+        window.location.href = "about:blank";
+
+    }
+</script>
+</head>
